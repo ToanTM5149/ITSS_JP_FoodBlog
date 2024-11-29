99b86abdebe29339304ca11d785ccc3d14a26b76
KLCuong <Cuong.KL215004@sis.hust.edu.vn>
2024-11-29 18:40:25 +0700

    Remake

--- client/src/ui/WatchBlog.js
+++ client/src/ui/WatchBlog.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../components/watchblog/Header";
+import Header from "../components/Header";
 import { fakePost } from "../components/watchblog/fakedata/fakedata";
 import "./WatchBlog.css";
 import { useState } from "react";
