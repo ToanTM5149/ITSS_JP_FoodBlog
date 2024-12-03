10d3d337523e7852b6fe483166aaa8e0aba8a402
ToanTM5149 <05.twt.09@gmail.com>
2024-12-03 10:54:32 +0700

    update be and create api

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // app.js
 const express = require('express');
+const cors = require('cors'); 
 const connectDB = require('./server/config/db');
 const userRoutes = require('./server/routes/user');
 
+app.use(cors())
 const app = express();
 
 connectDB();
