ce9bff22073ac047831efaf503fa5982050f77af
KLCuong <Cuong.KL215004@sis.hust.edu.vn>
2024-12-08 22:51:25 +0700

    Update again

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 // import DetailsBlog from "./DetailsBlog" (đường dẫn đến file đó)
 import Blog from "./ui/Blog";
 import BlogEditor from './ui/BlogEditor';
+import Watch from './ui/WatchBlog';
 function App() {
   return (
     <div className="App">
     
       {/* <DetailsBlog /> */} 
       {/* hoặc như này khi có 1 component được export với tên như vậy, và mình muốn hiển thị ở đây*/}
-      <Blog/>
+      {/* <Blog/> */}
+      <Watch/>
       {/* <Edit/> */}
       {/* <BlogEditor/> */}
     </div>
